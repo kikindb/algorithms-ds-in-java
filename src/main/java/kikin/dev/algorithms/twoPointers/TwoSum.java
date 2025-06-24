30f00318fc7a58cc7ae1f95dd018ea5bb37f93dc
= <=>
2025-06-24 11:31:01 -0500

    chore: twosum using a hashmap

--- src/main/java/kikin/dev/algorithms/twoPointers/TwoSum.java
+++ src/main/java/kikin/dev/algorithms/twoPointers/TwoSum.java
@@ -1,12 +1,15 @@
 package kikin.dev.algorithms.twoPointers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
   // given an array of sorted numbers, find 2 numbers that sum target value
-  public static int[] twoSum (int[] numbers, int target) {
+  public static int[] twoSumSorted (int[] numbers, int target) {
     int low = 0;
     int high = numbers.length - 1;
-    // {1,2,3,4,5,6}
+
     while (low < high) {
       int sum = numbers[low] + numbers[high];
       System.out.println(sum);
@@ -15,6 +18,21 @@ public class TwoSum {
       else high--;
     }
 
+    return new int[]{};
+  }
+
+  public static int[] twoSumHashMap (int [] nums, int target) {
+    Map<Integer, Integer> hashMap = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+
+      if (hashMap.containsKey(complement)) {
+        return new int[]{hashMap.get(complement), i};
+      }
+
+      hashMap.put(nums[i], i);
+    }
     return new int[]{};
   }
 
--- src/test/java/kikin/dev/algorithms/twoPointers/TwoSumTest.java
+++ src/test/java/kikin/dev/algorithms/twoPointers/TwoSumTest.java
@@ -8,9 +8,24 @@ public class TwoSumTest {
   @Test
   void shouldFindTwoSumValues() {
     int[] nums = {1,2,3,4,5,6};
-    Assertions.assertArrayEquals(new int[] {0,4}, TwoSum.twoSum(nums, 6));
+    Assertions.assertArrayEquals(new int[] {0,4}, TwoSum.twoSumSorted(nums, 6));
     int[] nums2 = {};
-    Assertions.assertArrayEquals(new int[] {}, TwoSum.twoSum(nums2, 6));
+    Assertions.assertArrayEquals(new int[] {}, TwoSum.twoSumSorted(nums2, 6));
+//    int[] nums3 = {3,2,4};
+//    Assertions.assertArrayEquals(new int[] {1,2}, TwoSum.twoSum(nums3, 6));
+  }
+
+  @Test
+  void shouldFindTwoSumValuesNotSorted() {
+    int[] nums = {1, 2, 3, 4, 5, 6};
+    int[] result = TwoSum.twoSumHashMap(nums, 6);
+    Assertions.assertEquals(6, nums[result[0]] + nums[result[1]]);
+    int[] nums2 = {};
+    Assertions.assertArrayEquals(new int[] {}, TwoSum.twoSumHashMap(nums2, 6));
+    int[] nums3 = {3,2,4};
+    Assertions.assertArrayEquals(new int[] {1,2}, TwoSum.twoSumHashMap(nums3, 6));
+    int[] nums4 = {2, 11, 15, 7};
+    Assertions.assertArrayEquals(new int[] {0,3}, TwoSum.twoSumHashMap(nums4, 9));
   }
 
 }
